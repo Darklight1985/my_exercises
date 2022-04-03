1caee9dc3f5ec6f9e90206d9e10789eb9655f171
Dmitrij Kolesnev <darklight1985@mail.ru>
2022-04-04 00:13:56 +0300

    OddCount

---
+++ src/main/java/exercises/loop/OddCount.java
@@ -0,0 +1,13 @@
+package exercises.loop;
+
+public class OddCount {
+    public static int count(int a, int b) {
+        int rsl = 0;
+       for (int i = a; i <= b; i++) {
+           if (i % 2 == 1) {
+               rsl++;
+           }
+       }
+       return rsl;
+    }
+}
